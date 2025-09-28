b7485ca009ddf6bdf5eb4aede17ca3e4e86d801d
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-09-29 00:19:05 +0300

    Create initial Simulation class

--- Simulation.h
+++ Simulation.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include "FrequencyRandomizer.h"
+
+class Simulation {
+public:
+    Simulation(const std::string& input_filename);
+    void run();
+
+private:
+    bool readData();
+    void performCalculations();
+    void printResults();
+
+    std::string m_filename;
+    int m_N;
+    std::vector<int> m_numbers;
+    std::vector<int> m_frequencies;
+    
+    std::map<int, int> m_actual_counts;
+    double m_max_deviation;
+};
